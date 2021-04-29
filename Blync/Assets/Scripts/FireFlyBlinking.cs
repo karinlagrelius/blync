27476a9fd6058ef9232d9ff2f9ce2409f0c42e4d
karin.lagrelius <karin.lagrelius@hotmail.com>
2021-04-29 13:50:27 +0200

    blinking works

--- Blync/Assets/Scripts/FireFlyBlinking.cs
+++ Blync/Assets/Scripts/FireFlyBlinking.cs
@@ -5,30 +5,22 @@ using UnityEngine;
 public class FireFlyBlinking : MonoBehaviour {
     // Start is called before the first frame update
     [SerializeField]
-    bool lit;
+    // bool lit;
 
     const int interval = 1;
     const int period = 8;
     float nextTime;
 
     public Material[] mats;
-    Material FlyLit; 
-    Material FlyUnLit;
-
+    Material flylit;
+    Material flyunlit;
     int timer;
-    //Random dice;
 
     void Start() {
-        mats = GetComponent<Renderer>().materials;
-        mats[0] = FlyUnLit; 
-        GetComponent<Renderer>().materials = mats; 
-
-        // mats[0] = Materials.Load("FlyLit", typeOf(Material)) as material; 
-        //mats[1] = Materials.Load("FlyUnLit", typeOf(Material)) as material; 
-        //gameObject.renderer.material = mats[1];
+        flyunlit = mats[0];
+        flylit = mats[1];
+        GetComponent<Renderer>().material = flyunlit;
         nextTime = 0;
-        //dice = new Random();
-        lit = false;
         timer = Random.Range(0, period);
     }
 
@@ -40,14 +32,10 @@ public class FireFlyBlinking : MonoBehaviour {
             nextTime += interval;
         }
         if(timer == 0) {
-            lit = true;
-            mats[0] = FlyLit; 
-            GetComponent<Renderer>().materials = mats; 
+            GetComponent<Renderer>().material = mats[1];
+
         } else if(timer == 1) {
-            lit = false;
-            mats[0] = FlyUnLit; 
-            GetComponent<Renderer>().materials = mats; 
-        } 
-        
+            GetComponent<Renderer>().material = mats[0];
+        }
     }
 }
